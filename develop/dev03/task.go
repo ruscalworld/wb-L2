de24a0a6391694c13c5a3c537d09be9faac43201
Nikolay <me@ruscal.world>
2024-01-28 21:35:09 +0300

    Develop 3

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита sort ===
 
@@ -25,6 +36,186 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+var (
+	// Индекс столбца, -1 для выбора строки целиком вне зависимости от кол-ва столбцов
+	columnIndex = flag.Int("k", -1, "column index")
 
+	// Использовать числовое значение вместо строкового
+	arithmeticValue = flag.Bool("n", false, "use arithmetic value")
+
+	// Сортировать в обратном порядке
+	reverseOrder = flag.Bool("r", false, "sort in reverse order")
+
+	// Оставить строки только с уникальными ключами
+	//
+	// Из man sort:
+	// -u, --unique
+	//         Unique keys.  Suppress all lines that have a key that is equal to an already processed one.
+	// Реализовано именно такое поведение - исключение дубликатов по ключу, а не по содержанию оригинальной строки.
+	unique = flag.Bool("u", false, "keep only unique lines")
+)
+
+// FileHolder хранит информацию о данных, которые были получены из файла, а также настройки для его сортировки.
+// Реализует sort.Interface, за счёт чего экземпляр этой структуры может быть передан в sort.Sort.
+type FileHolder struct {
+	lines []string
+
+	columnIndex     int
+	arithmeticValue bool
+	reverseOrder    bool
+	unique          bool
+}
+
+// NewFileHolder создаёт новый пустой FileHolder. Для работы требуется далее вызвать метод FileHolder.ReadLines.
+func NewFileHolder() *FileHolder {
+	return &FileHolder{
+		columnIndex:     *columnIndex,
+		arithmeticValue: *arithmeticValue,
+		reverseOrder:    *reverseOrder,
+		unique:          *unique,
+	}
+}
+
+// ReadLines считывает построчно данные из переданного reader и сохраняет строки в FileHolder.
+func (h *FileHolder) ReadLines(reader io.Reader) {
+	h.lines = make([]string, 0)
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		h.lines = append(h.lines, scanner.Text())
+	}
+}
+
+// WriteOutput записывает в переданный writer отсортированные данные, находящиеся в FileHolder.
+func (h *FileHolder) WriteOutput(writer io.Writer) (n int, err error) {
+	uniqueKeys := make(map[any]bool, h.Len())
+
+	for _, line := range h.lines {
+		k := h.Key(line)
+		// Проверяем, не записывали ли мы до этого строку с таким же ключом. Если передан флаг, требующий уникальности
+		// каждой строки в выходных данных, то пропускаем текущую строку.
+		if _, ok := uniqueKeys[k]; ok && h.unique {
+			continue
+		}
+
+		// Записываем текущую строку
+		m, err := writer.Write([]byte(line + "\n"))
+		n += m
+		if err != nil {
+			return n, err
+		}
+
+		uniqueKeys[k] = true
+	}
+
+	return
+}
+
+func (h *FileHolder) Len() int {
+	return len(h.lines)
+}
+
+// Less сравнивает строки с индексами i и j с учётом заданных параметров сортировки. Возвращается true, если элемент с
+// индексом i должен стоять перед элементом с индексом j.
+func (h *FileHolder) Less(i, j int) bool {
+	// Получаем ключи для каждой из строк, которые мы будем непосредственно сравнивать
+	a := h.Key(h.lines[i])
+	b := h.Key(h.lines[j])
+
+	// Если требуется отсортировать в обратном порядке, достаточно поменять ключи местами
+	if h.reverseOrder {
+		a, b = b, a
+	}
+
+	// Приводим значения ключей к конкретным типам, чтобы иметь возможность сравнить их через операцию "<"
+	switch a.(type) {
+	case string:
+		return a.(string) < b.(string)
+	case float64:
+		return a.(float64) < b.(float64)
+	default:
+		panic("unsupported types")
+	}
+}
+
+// Swap меняет местами элементы на позициях i и j
+func (h *FileHolder) Swap(i, j int) {
+	h.lines[i], h.lines[j] = h.lines[j], h.lines[i]
+}
+
+// Key получает ключ, который будет использоваться непосредственно для сравнения элементов при сортировке, для строки
+// input. Учитывает возможность разбиения на столбцы и использования в качестве ключа числового значения вместо строкового.
+func (h *FileHolder) Key(input string) any {
+	// Если задан определённый индекс столбца, по которому нужно сортировать, разбиваем входную строку на столбцы и
+	// выбираем столбец с нужным индексом. Если индекс находится вне границ массива, игнорируем разбиение по столбцам и
+	// работаем со всей строкой целиком.
+	if h.columnIndex >= 0 {
+		columns := strings.Split(input, " ")
+		if h.columnIndex < len(columns) {
+			input = columns[h.columnIndex]
+		}
+	}
+
+	// Если требуется использовать числовое значение вместо строкового, то преобразуем входную строку в число. Если
+	// преобразование не удаётся, используем в качестве ключа 0.
+	if h.arithmeticValue {
+		float, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			return 0.0
+		}
+
+		return float
+	}
+
+	return input
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1 {
+		fmt.Println("filename not provided")
+		os.Exit(1)
+		return
+	}
+
+	// Открываем файл с переданным через аргументы названием
+	file, err := os.Open(args[0])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+		return
+	}
+
+	// Закрываем файл при выходе из программы
+	defer file.Close()
+
+	// Инициализируем FileHolder
+	s := NewFileHolder()
+	s.ReadLines(file)
+
+	// Осуществляем сортировку
+	sort.Sort(s)
+
+	// По умолчанию выводим в stdout.
+	// Если передано два аргумента, то считаем последний из них названием выходного файла.
+	out := os.Stdout
+	if len(args) >= 2 {
+		file, err := os.Create(args[1])
+		if err != nil {
+			fmt.Println("unable to open output file:", err)
+		} else {
+			out = file
+			defer file.Close()
+		}
+	}
+
+	// Записываем выходные данные
+	_, err = s.WriteOutput(out)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(3)
+		return
+	}
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,90 @@
 package main
+
+import (
+	"bytes"
+	"sort"
+	"testing"
+)
+
+const input = "5 0\n10 1\n4 2\n3 3\n3 3\n2 4\n2 5"
+
+type testCase struct {
+	input    string
+	expected string
+	holder   *FileHolder
+}
+
+func TestFileHolder(t *testing.T) {
+	tests := []testCase{
+		{
+			holder: &FileHolder{
+				columnIndex: -1,
+			},
+			input:    input,
+			expected: "10 1\n2 4\n2 5\n3 3\n3 3\n4 2\n5 0\n",
+		},
+		{
+			holder: &FileHolder{
+				columnIndex: -1,
+				unique:      true,
+			},
+			input:    input,
+			expected: "10 1\n2 4\n2 5\n3 3\n4 2\n5 0\n",
+		},
+		{
+			holder: &FileHolder{
+				columnIndex:  -1,
+				reverseOrder: true,
+			},
+			input:    input,
+			expected: "5 0\n4 2\n3 3\n3 3\n2 5\n2 4\n10 1\n",
+		},
+		{
+			holder: &FileHolder{
+				columnIndex:  -1,
+				reverseOrder: true,
+				unique:       true,
+			},
+			input:    input,
+			expected: "5 0\n4 2\n3 3\n2 5\n2 4\n10 1\n",
+		},
+		{
+			holder: &FileHolder{
+				columnIndex: 1,
+			},
+			input:    input,
+			expected: "5 0\n10 1\n4 2\n3 3\n3 3\n2 4\n2 5\n",
+		},
+		{
+			holder: &FileHolder{
+				columnIndex: 1,
+				unique:      true,
+			},
+			input:    input,
+			expected: "5 0\n10 1\n4 2\n3 3\n2 4\n2 5\n",
+		},
+		{
+			holder: &FileHolder{
+				columnIndex:  1,
+				reverseOrder: true,
+			},
+			input:    input,
+			expected: "2 5\n2 4\n3 3\n3 3\n4 2\n10 1\n5 0\n",
+		},
+	}
+
+	for i, c := range tests {
+		c.holder.ReadLines(bytes.NewReader([]byte(c.input)))
+		sort.Sort(c.holder)
+
+		buf := &bytes.Buffer{}
+		_, err := c.holder.WriteOutput(buf)
+		if err != nil {
+			t.Errorf("error in test %d: %s", i, err)
+		}
+
+		if buf.String() != c.expected {
+			t.Errorf("unexpected value in test %d:\n %s", i, buf.String())
+		}
+	}
+}
