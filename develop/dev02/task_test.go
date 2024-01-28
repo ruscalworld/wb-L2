4435895cba1ae29cb29a9de22ac44f75dadd0e7b
Nikolay <me@ruscal.world>
2024-01-28 13:53:34 +0300

    Develop 2

--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,22 @@
 package main
+
+import "testing"
+
+func TestUnpack(t *testing.T) {
+	cases := map[string]string{
+		"a4bc2d5e": "aaaabccddddde",
+		"abcd":     "abcd",
+		"45":       "",
+		"":         "",
+		`qwe\4\5`:  "qwe45",
+		`qwe\45`:   "qwe44444",
+		`qwe\\5`:   `qwe\\\\\`,
+	}
+
+	for input, expected := range cases {
+		actual, _ := Unpack(input)
+		if expected != actual {
+			t.Errorf("unexpected result for input \"%s\": \"%s\" (expected \"%s\")", input, actual, expected)
+		}
+	}
+}
