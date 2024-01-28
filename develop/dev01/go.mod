28ac0e44946f5c27ebfb589acb0c3976e188a308
Nikolay <me@ruscal.world>
2024-01-28 13:28:10 +0300

    Develop 1

---
+++ develop/dev01/go.mod
@@ -0,0 +1,13 @@
+module dev01
+
+go 1.21.0
+
+require (
+	github.com/beevik/ntp v1.3.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+	"github.com/urfave/cli/v2"
+)
+
 /*
 === Базовая задача ===
 
@@ -13,5 +22,29 @@ package main
 */
 
 func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "server",
+				Value: "0.beevik-ntp.pool.ntp.org",
+			},
+		},
 
+		Action: func(ctx *cli.Context) error {
+			t, err := ntp.Time(ctx.String("server"))
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Current time:", t.Format(time.RFC1123))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+		return
+	}
 }
