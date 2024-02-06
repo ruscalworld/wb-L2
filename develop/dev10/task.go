97b75768844e3a741e0ef22289776cf9e187aae6
Nikolay <me@ruscal.world>
2024-02-06 17:36:54 +0300

    Develop 10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +27,83 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+var rawTimeout = flag.String("timeout", "10s", "timeout")
 
+func initSession(timeout time.Duration, address string) error {
+	// Подключаемся к серверу
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return err
+	}
+
+	// Сообщаем об успешном подключении и создаём контекст
+	fmt.Println("Connected to", address)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Запускаем два "пайпа" для перенаправления данных из stdin в соединение и из соединения в stdout
+	go pipe(ctx, cancel, conn, os.Stdin)
+	go pipe(ctx, cancel, os.Stdout, conn)
+
+	// Ожидаем завершения работы пайпов
+	<-ctx.Done()
+	return nil
+}
+
+func pipe(ctx context.Context, cancel context.CancelFunc, dst io.Writer, src io.Reader) {
+	// Будем использовать reader из bufio
+	reader := bufio.NewReader(src)
+
+	// Если что-то случится с пайпом, сообщаем об этом наверх и через контекст завершаем работу всей программы
+	defer cancel()
+
+	for {
+		select {
+		// Обрабатываем сигнал завершения работы программы
+		case <-ctx.Done():
+			return
+
+		// Если сигнала о завершении работы нет, то продолжаем работу
+		default:
+			// Читаем строку до переноса
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				// Здесь может возникнуть io.EOF, спровоцированный нажатием Ctrl+D, он будет обработан так же, как и
+				// остальные ошибки.
+				cancel()
+				return
+			}
+
+			// Отправляем считанную строку в writer
+			_, err = dst.Write([]byte(line))
+			if err != nil {
+				cancel()
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() < 2 {
+		fmt.Println("not enough arguments")
+		return
+	}
+
+	// Преобразуем строку, полученную через флаг --timeout в time.Duration.
+	timeout, err := time.ParseDuration(*rawTimeout)
+	if err != nil {
+		fmt.Println("invalid timeout:", err)
+		return
+	}
+
+	// Формируем адрес из аргументов команды (первые два аргумента, разделённые через ":" = адрес:порт)
+	address := strings.Join(flag.Args()[:2], ":")
+
+	// Инициализируем сессию
+	err = initSession(timeout, address)
+	if err != nil {
+		fmt.Println("error initiating session:", err)
+		return
+	}
 }
